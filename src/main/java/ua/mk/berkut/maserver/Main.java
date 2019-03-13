ec5507a29358493c5bbd2cbcdc03166240fd5584
EugenyB <eberkunsky@gmail.com>
2019-03-13 19:47:03 +0200

    added properties

--- src/main/java/ua/mk/berkut/maserver/Main.java
+++ src/main/java/ua/mk/berkut/maserver/Main.java
@@ -1,18 +1,23 @@
 package ua.mk.berkut.maserver;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.util.Properties;
 
 public class Main {
+    Connection connection;
+
     public static void main(String[] args)throws Exception {
         new Main().run();
     }
 
-    private void run() throws SQLException {
-        Connection connection = DriverManager.getConnection(
-                "jdbc:mariadb://localhost:3306/mainacademy2",
-                "eugeny",
-                "123");
+    private void run() throws Exception {
+        Properties properties = new Properties();
+        properties.load(Files.newBufferedReader(Paths.get("chat.cfg")));
+        connection = DriverManager.getConnection(
+                properties.getProperty("url"),
+                properties);
     }
 }
